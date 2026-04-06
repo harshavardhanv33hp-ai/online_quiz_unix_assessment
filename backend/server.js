ab5b1f72d2bea1891556a7b1157e1fcce9615401
Harshavardhan <harshavardhanv33hp@gmail.com>
2026-04-06 18:53:29 +0530

    Setup Express backend server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Quiz Backend Running");
+  res.send("Quiz Backend Running 🚀");
 });
 
 app.listen(5000, () => {
