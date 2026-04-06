f58fbd084292a9fba0e90cb3ae7a9986c99764d9
Harshavardhan <harshavardhanv33hp@gmail.com>
2026-04-06 18:32:30 +0530

    Implement option selection using radio buttons

--- src/quiz.js
+++ src/quiz.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import questions from "./data";
 
 function Quiz() {
+  const [selected, setSelected] = useState("");
+
   return (
     <div>
       <h3>{questions[0].question}</h3>
 
       {questions[0].options.map((opt, index) => (
         <div key={index}>
+          <input
+            type="radio"
+            name="option"
+            value={opt}
+            checked={selected === opt}
+            onChange={() => setSelected(opt)}
+          />
           {opt}
         </div>
       ))}
