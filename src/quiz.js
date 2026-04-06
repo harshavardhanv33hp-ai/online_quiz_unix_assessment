8efc4a12dc0c3897f0fc2348d587f9c04129381f
Harshavardhan <harshavardhanv33hp@gmail.com>
2026-04-06 18:31:26 +0530

    Display first quiz question

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,12 @@
+import React from "react";
 import questions from "./data";
 
-<h3>{questions[0].question}</h3>
+function Quiz() {
+  return (
+    <div>
+      <h3>{questions[0].question}</h3>
+    </div>
+  );
+}
+
+export default Quiz;
