cc4de532c0753b5e9ddb44f683fa2bdca4a92543
Harshavardhan <harshavardhanv33hp@gmail.com>
2026-04-06 18:32:01 +0530

    Display quiz options

--- src/quiz.js
+++ src/quiz.js
@@ -5,6 +5,12 @@ function Quiz() {
   return (
     <div>
       <h3>{questions[0].question}</h3>
+
+      {questions[0].options.map((opt, index) => (
+        <div key={index}>
+          {opt}
+        </div>
+      ))}
     </div>
   );
 }
