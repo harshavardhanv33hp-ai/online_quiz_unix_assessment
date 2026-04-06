f640a49f55b1208d7c6343a8b238908a6bdb6f0e
Harshavardhan <harshavardhanv33hp@gmail.com>
2026-04-06 18:24:53 +0530

    add quiz questions data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    question: "Capital of India?",
+    options: ["Mumbai", "Delhi", "Chennai", "Kolkata"],
+    answer: "Delhi"
+  },
+  {
+    question: "HTML stands for?",
+    options: [
+      "Hyper Text Markup Language",
+      "High Text Machine Language",
+      "Hyper Tool Markup Language",
+      "None"
+    ],
+    answer: "Hyper Text Markup Language"
+  }
+];
+
+export default questions;
